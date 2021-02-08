7f83e729d4d992847859ae27a9aac4dcc9d32679
arflix <78759978+arflix@users.noreply.github.com>
2021-02-08 22:23:09 +0200

    Delete start.sh

--- start.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-[[ $TERM != "screen" ]] && exec tmux
-if command -v tmux &> /dev/null && [ -n "$PS1" ] && [[ ! "$TERM" =~ screen ]] && [[ ! "$TERM" =~ tmux ]] && [ -z "$TMUX" ]; then
-  exec tmux
-fi
