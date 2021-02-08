38208c3332b49bf3f50f79e0ce35e9a6f7205191
arflix <78759978+arflix@users.noreply.github.com>
2021-02-08 22:20:49 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+[[ $TERM != "screen" ]] && exec tmux
 if command -v tmux &> /dev/null && [ -n "$PS1" ] && [[ ! "$TERM" =~ screen ]] && [[ ! "$TERM" =~ tmux ]] && [ -z "$TMUX" ]; then
   exec tmux
 fi
