39b14c5f9354c2027b8b442ac35b5a82b6a0f4ee
arflix <78759978+arflix@users.noreply.github.com>
2021-02-08 22:23:57 +0200

    Add files via upload

---
+++ start.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+[[ $TERM != "screen" ]] && exec tmux
+
+if command -v tmux &> /dev/null && [ -n "$PS1" ] && [[ ! "$TERM" =~ screen ]] && [[ ! "$TERM" =~ tmux ]] && [ -z "$TMUX" ]; then
+  exec tmux
+fi
+
+!sudo tmux
