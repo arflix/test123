d646da93cd352a448f9dfd85c0009abe3818adb4
arflix <78759978+arflix@users.noreply.github.com>
2021-03-08 15:08:45 +0200

    Update katacoda.sh

--- katacoda.sh
+++ katacoda.sh
@@ -1,27 +1,13 @@
-#! /bin/bash
-printf "Check Out My Channel While Waiting- https://youtu.be/VeO22kIvZOQ" >&2
-{
-sudo useradd -m ALOK
-sudo adduser ALOK sudo
-echo 'ALOK:8426' | sudo chpasswd
-sed -i 's/\/bin\/sh/\/bin\/bash/g' /etc/passwd
 sudo apt update
 sudo apt -y install screen
 sudo apt -y install xfce4
-wget https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb
-sudo dpkg --install chrome-remote-desktop_current_amd64.deb
-sudo apt install --assume-yes --fix-broken
+sudo apt -y install firefox
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo dpkg --install google-chrome-stable_current_amd64.deb
 sudo apt install --assume-yes --fix-broken
-sudo apt -y install obs-studio
-sudo apt -y install firefox
-sudo adduser ALOK chrome-remote-desktop
-} &> /dev/null &&
-printf "\nSetup Completed " >&2 ||
-printf "\nError Occured " >&2
-printf '\nCheck https://remotedesktop.google.com/headless  Copy Command Of Debian Linux And Paste Down\n'
-read -p "Paste Here: " CRP
-su - ALOK -c """$CRP"""
-printf 'Check https://remotedesktop.google.com/access/ \n'
-printf 'Your SUDO Pasword Is 8426 \n'
+sudo apt install tightvncserver
+tightvncserver :1
+wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip
+unzip ngrok-stable-linux-amd64.zip
+sudo apt install blender default-jre
+wget -O client.jar https://www.sheepit-renderfarm.com/media/applet/client-latest.php
